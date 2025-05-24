14d5681ded9b633d340fe073ebbef3d85c7e812b
NoidHuma <nikitag.big2005@gmail.com>
2025-05-24 05:04:19 +0300

    lab7 refactor structure

---
+++ F#Project/lab7/lab7main.fs
@@ -0,0 +1,100 @@
+﻿module lab7main
+
+open System
+open GeometryShapes
+open Figures
+open Maybe
+open Agent
+open Parser
+open Passport
+
+
+let runLab7 =
+
+    // 1 задание
+    // let rectangle = new Rectangle(2.0, 6.0)
+    // let square = new Square(5.0)
+    // let circle = new Circle(4.5)
+
+    // printfn "Реализация метода ToString():"
+    // printfn "%s" (rectangle.ToString())
+    // printfn "%s" (square.ToString())
+    // printfn "%s" (circle.ToString())
+
+    // let printInformation (shape: IPrint) =
+    //     shape.Print()
+ 
+    // printfn "Реализация метода интерфейса:"
+    // printInformation rectangle
+    // printInformation square
+    // printInformation circle
+
+    // printfn "Площадь прямоугольника: %.2f" (areaShape rectangle)
+    // printfn "Площадь квадрата: %.2f" (areaShape square)
+    // printfn "Площадь круга: %.2f" (areaShape circle)
+
+    // 2 задание
+    // checkFunctorLaws()
+    // checkApplicativeLaws()
+    // checkMonadLaws()
+
+    // 3 задание
+    // let expressions = [
+    //     "1 + 2"
+    //     "3 * (4 + 5)"
+    //     "6 / 2 - 1"
+    //     "10 + 2 * 3"
+    // ]
+    // for exp in expressions do
+    //     printfn "\nВход: %s" exp
+    //     parse exp |> ignore
+
+    // 4 задание
+    // agent.Post(Print "Привет, агент!")
+    // agent.Post(ShowTime)
+    // agent.Post(Print "Ещё одно сообщение")
+    // agent.Post(Exit)
+    // agent.PostAndAsyncReply(fun replyChannel ->
+    //     replyChannel.Reply()
+    //     Exit
+    // )
+    // |> Async.RunSynchronously
+    // Console.ReadKey() |> ignore
+
+    // 5 задание
+    let vehicle = VehiclePassport(
+        vin = "1HGBH41JXMN109186",
+        mark = "Toyota",
+        model = "Corolla",
+        category = "Легковой",
+        year = 2015,
+        engineModelAndNumber = "2ZR-FE12345",
+        color = "Красный",
+        maxAllowedWeight = 1500.0,
+        exportCountry = "Япония"
+    )
+    vehicle.PrintDocument()
+
+    if vehicle.Validate() then
+        printfn "Данные о транспортном средстве валидны."
+    else
+        printfn "Ошибка: Недопустимые данные о транспортном средстве."
+
+    let vehicle2 = VehiclePassport(
+        vin = "1HGBH41JXMN109187",  // другой VIN
+        mark = "Honda",
+        model = "Civic",
+        category = "Легковой",
+        year = 2017,
+        engineModelAndNumber = "K24Z12345",
+        color = "Синий",
+        maxAllowedWeight = 1450.0,
+        exportCountry = "Япония"
+    )
+
+    if vehicle.Equals(vehicle2) then
+        printfn "Транспортные средства одинаковые."
+    else
+        printfn "Транспортные средства разные."
+
+    0
--- F#Project/main.fs
+++ F#Project/main.fs
@@ -1,6 +1,7 @@
 ﻿open System
 open lab5main
 open lab6main
+open lab7main
 
 
 [<EntryPoint>]
@@ -8,5 +9,6 @@ let main argv =
     Console.WriteLine("Lab 5")
     runLab5
     runLab6
+    runLab7
 
     0
