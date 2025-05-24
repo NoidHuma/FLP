0b5b5289c11b937f58c3b6c836ccf292ac95345d
NoidHuma <nikitag.big2005@gmail.com>
2025-05-24 04:59:49 +0300

    lab7 agent

---
+++ F#Project/lab7/Agent.fs
@@ -0,0 +1,24 @@
+﻿module Agent
+open System
+
+type AgentMessage =
+    | Print of string
+    | ShowTime
+    | Exit     
+
+let agent = MailboxProcessor.Start(fun inbox ->
+    let rec loop () = async {
+        let! msg = inbox.Receive()
+        match msg with
+        | Print text ->
+            printfn "Сообщение: %s" text
+            return! loop()
+        | ShowTime ->
+            let now = DateTime.Now
+            printfn "Текущее время: %s" (now.ToString("HH:mm:ss"))
+            return! loop()
+        | Exit ->
+            printfn "Агент завершает работу."
+    }
+    loop ()
+)
