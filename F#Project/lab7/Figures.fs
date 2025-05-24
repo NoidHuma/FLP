370846cf4054b059628bc0cb77eaad97110804d9
NoidHuma <nikitag.big2005@gmail.com>
2025-05-24 04:55:59 +0300

    lab7 figures

---
+++ F#Project/lab7/Figures.fs
@@ -0,0 +1,12 @@
+﻿module Figures
+ 
+type Figure =
+    | Rectangle of width: float * height: float
+    | Square of sideLength: float
+    | Circle of radius: float
+
+let areaShape (figure: Figure) : float =
+    match figure with
+    | Rectangle (width, height) -> width * height
+    | Square sideLength -> sideLength * sideLength
+    | Circle radius -> System.Math.PI * radius * radius
---
+++ F#Project/lab7/GeometryShapes.fs
@@ -0,0 +1,43 @@
+﻿module GeometryShapes
+open System
+
+type IPrint =
+    abstract member Print: unit -> unit
+ 
+[<AbstractClass>]
+type GeomShape() =
+    // Виртуальный метод для вычисления площади
+    abstract member Area : unit -> float
+    default this.Area() = 0.0
+
+
+type Rectangle(width: float, height: float) =
+    inherit GeomShape()
+    member this.Width = width
+    member this.Height = height
+
+    override this.Area() = this.Width * this.Height
+
+    override this.ToString() = sprintf "Прямоугольник (ширина: %.2f, высота: %.2f, площадь: %.2f)" width height (this.Area())
+
+    interface IPrint with
+        member this.Print() = printfn "%s" (this.ToString())
+
+type Square(sideLength: float) =
+    inherit Rectangle(sideLength, sideLength)
+    
+    override this.ToString() = sprintf "Квадрат (длина стороны: %.2f, площадь: %.2f)" sideLength (this.Area())
+
+    interface IPrint with
+        member this.Print() = printfn "%s" (this.ToString())
+
+type Circle(radius: float) =
+    inherit GeomShape()
+    member this.Radius = radius
+
+    override this.Area() = Math.PI * this.Radius * this.Radius
+
+    override this.ToString() = sprintf "Круг (радиус: %.2f, площадь: %.2f)" this.Radius (this.Area())
+
+    interface IPrint with
+        member this.Print() = printfn "%s" (this.ToString())
